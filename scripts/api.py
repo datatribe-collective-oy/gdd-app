61b5b082498ecf89de16f982f4df31962913c858
eveliinahampus <t2haev00@students.oamk.fi>
2025-04-29 09:56:33 +0300

    feat: add gdd and weather endpoints

--- scripts/api.py
+++ scripts/api.py
@@ -7,6 +7,14 @@ app = FastAPI()
 def read_root():
     return ("Hello World")
 
+@app.get("/api/gdd/")
+def get_gdd():
+    return {"message": "Get GDD"}
+
+@app.get("/api/weather/")
+def get_weather():
+    return {"message": "Estimate Weather"}
+
 # Run API by uvicorn, local development. 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
