59bb02b482467ce92870ab6a4372afeccf9b48ca
eveliinahampus <t2haev00@students.oamk.fi>
2025-05-04 14:14:53 +0300

    feat: stable infra - ec2 instance with docker, security group, s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,13 +38,16 @@ resource "aws_instance" "gdd_server" {
   ami           = "ami-0dd574ef87b79ac6c" # amazon linux 2023
   instance_type = "t3.micro"
 
+  user_data = file("${path.module}/user_data/ec2-docker.sh")
+
+
   tags = {
     Name    = "gdd-server"
     Project = "GDD-Timing-System"
   }
 }
 
-#TODO make bucket private
+
 # Layer 4: Data Storage 
 resource "aws_s3_bucket" "gdd_raw_data" {
   bucket        = "gdd-raw-weather-data"
@@ -55,7 +58,16 @@ resource "aws_s3_bucket" "gdd_raw_data" {
   }
 }
 
-# Prints IP and DNS values on CLI terraform apply or terraform output.
+resource "aws_s3_bucket_public_access_block" "gdd_raw_data_block" {
+  bucket = aws_s3_bucket.gdd_raw_data.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Prints IP and DNS values on CLI when applying terraform apply or terraform output.
 output "gdd_server_public_ip" {
   value = aws_instance.gdd_server.public_ip
 }
