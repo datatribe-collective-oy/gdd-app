29cf4df764c129c9380f1e5520cd18022337f4b7
eveliinahampus <t2haev00@students.oamk.fi>
2025-05-08 13:13:40 +0300

    chore: update variables

--- init-airflow.sh
+++ init-airflow.sh
@@ -5,10 +5,10 @@ airflow db upgrade
 if ! airflow users list | grep -q "${AIRFLOW_ADMIN_USER}"; then
     airflow users create \
     --username "${AIRFLOW_ADMIN_USER}" \
-    --firstname Admin \
-    --lastname User \
-    --role Admin \
-    --email admin@example.com \
+    --firstname "${AIRFLOW_ADMIN_FIRSTNAME}" \
+    --lastname "${AIRFLOW_ADMIN_LASTNAME}" \
+    --role ${AIRFLOW_ADMIN_ROLE} \
+    --email ${AIRFLOW_ADMIN_EMAIL} \
     --password "${AIRFLOW_ADMIN_PASSWORD}"
 else
     echo "Admin user ${AIRFLOW_ADMIN_USER} already exists."
